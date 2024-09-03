103db9c8fd8987d516ada71dab3a16c3f038ec96
Carsten Behring <carsten.behring@gmail.com>
2024-09-03 21:22:27 +0200

    Update onnx.clj

--- projects/ml/onnx/notebooks/onnx.clj
+++ projects/ml/onnx/notebooks/onnx.clj
@@ -4,6 +4,8 @@
   (:import [ai.onnxruntime OrtEnvironment OrtSession$SessionOptions
             OnnxTensor]))
 
+;; # Using ONNX models from Clojure
+;;
 ;; We assume that we get an ONNX file from somewhere and want to use it from Clojure.
 ;; ONNX files can for example be trained with Python sklearn and exported to ONNX format.
 ;; There are as well model zoos existing, which allow to download pre-trained models.
@@ -11,6 +13,7 @@
 ;; We can open such file using the JAVA ONNX run-time with has the maven coordinates
 ;; com.microsoft.onnxruntime/onnxruntime {:mvn/version "1.19.0"}
 
+;; ## Load and inspect ONNX file
 ;; We use here a model which was trained on the well know iris data and can predict the species
 (def env (OrtEnvironment/getEnvironment))
 (def session (.createSession env "logreg_iris.onnx"))
@@ -39,6 +42,7 @@
 ;; Now we need to construct an instance of ai.onnxruntime.OnnxTensor of shape [-1,4]
 ;; This can be done starting from a vector-of-vector, for example
 
+;; ## Run inference on arrays
 (def input-data
   [[7   0.5  0.5 0.5]
    [0.5 1    1     1]])
@@ -67,7 +71,8 @@ prediction
  #(.getValue %)
  (->  prediction second val .getValue))
 
-;; In case we ave our data to be used in a tech.ml.dataset
+;; ## Run inference on tech.dataset
+;; In case we have our data in a tech.ml.dataset
 
 (def ds
   (tc/dataset [[0.5 0.5 0.5 0.5]
@@ -84,10 +89,13 @@ prediction
       env 
      (into-array (map float-array (tc/rows ds)))))
 
-;; Running predictions is then te same.
+;; Running predictions is then the same.
 (def prediction-2 (.run session {"float_input" tensor-2}))
 (.. prediction-2 (get 0) getValue)
 
-;; Overall we an use any ONNX model from Clojure.
+;; Overall we can use any ONNX model from Clojure.
 ;; This allows polyglot scenarios where data preprocession and model evaluation
-;; is done in Clojure, while training is done in Python which its huge ecosystem of models.
+;; is done in Clojure, while training is done in Python with its huge ecosystem of ML models.
+;;
+;; Hoefuly overtime the ONNX standard will see widespread use. Most sklearn models/pipelines can be exported 
+;; to ONNX using [sklear-onnx](https://onnx.ai/sklearn-onnx/)
