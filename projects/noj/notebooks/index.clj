e04c1d8bbbeee63c1ace5b422a417bf204c6f083
Carsten Behring <carsten.behring@gmail.com>
2024-09-07 23:20:14 +0200

    in noj

--- projects/noj/notebooks/index.clj
+++ projects/noj/notebooks/index.clj
@@ -8,3 +8,4 @@
 ;; is an opinionated way to use the emerging Clojure data stack.
 ;; It collects a few of the relevant libraries together
 ;; and addes some layers of composition on top of them.
+;;
