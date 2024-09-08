f10a0c5e1919f5006529b88f998af745ff14e2cc
Carsten Behring <carsten.behring@gmail.com>
2024-09-08 21:26:10 +0200

    removed book

--- projects/ml/sklearn-clj/render.clj
+++ projects/ml/sklearn-clj/render.clj
@@ -4,11 +4,8 @@
 (clay/make! {:format [:quarto :html]
              :show false
              :base-source-path "notebooks"
-             :source-path ["index.clj"
-                           
-                           ]
+             :source-path ["index.clj"]
              :base-target-path "docs"
-             :book {:title "Use sklearn-clj "}
              :clean-up-target-dir true})
 (System/exit 0)
 
