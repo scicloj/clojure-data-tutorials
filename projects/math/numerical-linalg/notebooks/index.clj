eeb4db7e2e049309579fb7f3613f352ea599afde
daslu <daniel.slutsky@gmail.com>
2024-12-25 23:40:48 +0200

    numerical linear algebra - WIP

---
+++ projects/math/numerical-linalg/notebooks/index.clj
@@ -0,0 +1,10 @@
+(load-string (slurp  "https://raw.githubusercontent.com/scicloj/clojure-data-tutorials/main/header.edn"))
+
+^:kindly/hide-code
+(ns index)
+
+;; # Preface
+
+;; This is a Clojure port of parts of the Fast.ai Numerical Linear Algebra
+;; v2 course.
+;; [fastai](https://github.com/fastai)/[numerical-linear-algebra-v2](https://github.com/fastai/numerical-linear-algebra-v2)
---
+++ projects/math/numerical-linalg/notebooks/movie/README.md
@@ -0,0 +1,3 @@
+Video_003.mp4 was downloaded from 
+https://nbviewer.org/github/fastai/numerical-linear-algebra-v2/blob/master/nbs/02-Background-Removal-with-SVD.ipynb
+on 2024-12-24.
